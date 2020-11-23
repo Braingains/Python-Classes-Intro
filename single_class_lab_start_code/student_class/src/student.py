0a97bc5db1fc78b351de22a6ace8f9baf0753a7d
Ewan Brain <ewan.brain@gmail.com>
2020-11-23 15:03:29 +0000

    part a complete

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,17 +1,29 @@
-class student:
+class Student:
 
-    def __init__(self, student, cohort):
-        self.student= student
+    def __init__(self, name, cohort):
+        self.name = name
         self.cohort = cohort
 
-    def talk():
+    def talk(self):
         return("I can talk!")
 
-    def say_favourite_language(language):
+    def say_favourite_language(self, language):
         return(f"I love {language}")
 
+    def student_has_name(student):
+        return(student)
 
-def student_has_name(student):
-    return(student)
+    def student_has_cohort(cohort):
+        return(cohort)
 
+    def student_can_update_name(new_name):
+        return(new_name)
 
+    def student_can_change_cohort(new_cohort):
+        return(new_cohort)
+
+    def student_can_talk():
+        return talk()
+
+    def test_student_has_favourite_language(language):
+        return say_favourite_language()
